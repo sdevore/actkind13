03a3b0617f9dd08635bd4f1024d8e7d57745d713
Sam DeVore <sdevore@gmail.com>
2026-05-23 16:48:11 -0700

    layout fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,6 @@
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <livewire:appreciates.recents class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
-
         </div>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <livewire:acts.public-recents class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
